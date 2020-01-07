a069c1baa569b0b5e2a22deebf12b5a3ddae6106
Inner Exception <inner.exception@outlook.com>
2020-01-07 18:18:27 +0100

    completed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDie;
+  int rightDie;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    rollDice();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDie.png'),
+              onPressed: () => rollDice(),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDie.png'),
+              onPressed: () => rollDice(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void rollDice() {
+    setState(() {
+      leftDie = Random().nextInt(6) + 1;
+      rightDie = Random().nextInt(6) + 1;
+    });
   }
 }
